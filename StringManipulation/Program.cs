38728d9e72cd82c210de045ff647b85c331a7e91
test <k.widlak@microsoft.wsei.edu.pl>
2019-01-31 17:56:49 +0100

    Praca ze stringami

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -10,6 +10,34 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
+            string text = "Ktury ipsum inny tekst";
+            //text = text.ToUpper();
+            //text = text.ToLower();
+            //text = text.Substring(0, 5);
+            //var arr = text.Split(' ')[0]; //split dzieli string na tablicę znaków (char)
+
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string[] arr2 = text.Split(' ');
+            //text = string.Join("_", arr2);
+            //text = text.Replace("ipsum ", "");
+            //text = text.Remove(7, 6);
+            text = "Super text" + text;
+
+            text = $"Super tekst {text}"; //zmienna tworzona tylko raz w pamięci w odróżnieniu od poprzedniego - dobre do krótkich stringów
+
+            StringBuilder stringBuilder = new StringBuilder(text);
+            stringBuilder.Append(" trochę późno ");
+            stringBuilder.AppendLine(" ale nadal jasno ");
+            stringBuilder.AppendLine(" ale nadal jasno ");
+            stringBuilder.Replace("nadal", "coś");
+
+            Console.WriteLine(stringBuilder.ToString());
+            Console.Read();
+
         }
     }
 }
